9438f930b39c984f4f6e9d4f388b8ac5d2e1a9fb
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-09-16 21:37:17 +0530

    Build Login Window :)

--- src/views/UserView.java
+++ src/views/UserView.java
@@ -1,7 +1,21 @@
 package views;
 
-public class UserView {
-    public static void main(String[] args) {
+import javax.swing.*;
+import java.awt.*;
 
+public class UserView extends JFrame {
+
+    public UserView() throws HeadlessException {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 500);
+        setResizable(false);
+        setTitle("Login");
+//        setLocation(500, 150);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        UserView userView = new UserView();
     }
 }
