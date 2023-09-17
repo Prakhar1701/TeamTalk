59a5bcc5c014b645016db7c17c5ab294b472fcb8
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-09-17 13:05:22 +0530

    Event Binding

--- src/views/UserView.java
+++ src/views/UserView.java
@@ -2,10 +2,14 @@ package views;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class UserView extends JFrame {
+    int counter;
 
     public UserView() throws HeadlessException {
+        counter = 0;
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(500, 500);
         setResizable(false);
@@ -19,6 +23,17 @@ public class UserView extends JFrame {
         container.setLayout(null);
         welcome.setBounds(100, 70, 200, 60);
         container.add(welcome);
+
+        JButton button = new JButton("Count"); //Source
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                welcome.setText("Count: " + ++counter);
+            }
+        });
+        button.setBounds(100, 300, 200, 50);
+        container.add(button);
+
         setVisible(true);
     }
 
