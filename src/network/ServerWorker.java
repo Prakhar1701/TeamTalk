b4a0524c2cff97a994b1059fd400d802d73143a9
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-09-27 01:00:44 +0530

    Reading and Writing Data on Stream

--- src/network/ServerWorker.java
+++ src/network/ServerWorker.java
@@ -1,8 +1,6 @@
 package network;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
 public class ServerWorker extends Thread {
@@ -19,6 +17,26 @@ public class ServerWorker extends Thread {
 
     @Override
     public void run() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String line;
 
+        try {
+            while (true) {
+                line = br.readLine();
+                if (line.equalsIgnoreCase("quit")) break;
+                out.write(line.getBytes());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+                if (in != null) in.close();
+                if (out != null) out.close();
+                if (clientSocket != null) clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
